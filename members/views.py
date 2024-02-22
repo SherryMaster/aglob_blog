38a620dedb24120019ed756593e1b29e05ef0711
SherryMaster <128972759+SherryMaster@users.noreply.github.com>
2024-02-22 12:35:37 +0500

    formatted register form, Creator's post will show 'you' in blue to them.

--- members/views.py
+++ members/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
-
+from .forms import UserRegisterForm
 
 # Create your views here.
 class SignUp(CreateView):
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
 
